4e067ee88598ea29ef632f3d55220cbf9c56a7e8
SeannZhou <seanzhou0@gmail.com>
2016-10-19 17:24:48 -0400

    Finished program

---
+++ Decimal_to_Binary.cpp
@@ -0,0 +1,31 @@
+/*
+	Sean Zhou
+	Description: converting decimal numbers to binary.
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() { 
+	int decimal;	//Input
+	int digit;	//Used to break number down and convert
+	string result;	//Output after concatenating 
+
+	cout << "Decimal: ";
+	cin >> decimal;
+
+	while(decimal != 0) {		//Loop until decimal is completely broken down
+		digit = decimal % 2;	//Mod and use remainder to make binary
+		if(digit == 1)		//Concatenate to make result
+			result += "1";
+		else
+			result += "0";
+		
+		decimal /= 2;		//Breaking down
+	}
+	
+	cout << "Binary: " + result << endl;	//Output
+
+	return 0;
+}
