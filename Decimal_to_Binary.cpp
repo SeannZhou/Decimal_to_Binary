2bacdfbcb9c1e2cbdb15322aa7a83af3492dce8a
SeannZhou <seanzhou0@gmail.com>
2016-10-20 23:17:52 -0400

    Fixed reversed output bug

--- Decimal_to_Binary.cpp
+++ Decimal_to_Binary.cpp
@@ -18,9 +18,9 @@ int main() {
 	while(decimal != 0) {		//Loop until decimal is completely broken down
 		digit = decimal % 2;	//Mod and use remainder to make binary
 		if(digit == 1)		//Concatenate to make result
-			result += "1";
+			result = "1" + result;
 		else
-			result += "0";
+			result = "0" + result;
 		
 		decimal /= 2;		//Breaking down
 	}
